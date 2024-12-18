ee70cdeaa92924622e64e1074b2c8e92994f431a
Sonukamble <ddkamble2141@gmail.com>
2024-12-18 21:02:52 +0530

    added HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,42 @@
+import { HashRouter, Routes, Route, BrowserRouter } from 'react-router-dom';
 import UserInformationProvider from "./store/Login-context";
-import Services from "./components/File/Services";
-import Navbar from "./components/Navbar/Navbar"
+import Navbar from "./components/Navbar/Navbar";
 import LogIn from "./components/Pages/LogIn";
 import SignUp from "./components/Pages/SignUp";
-import './css/App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from "./components/Pages/About";
 import Home from "./components/Pages/Home";
+import Services from "./components/File/Services";
+import './css/App.css';
 
 function App() {
-  // const FastAPI = process.env.REACT_APP_API_URL;
+  const isProduction = window.location.hostname !== 'localhost'; // Or use another method to check
 
   return (
     <UserInformationProvider>
-
       <Navbar />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/signup" element={<SignUp />} />
-
-        </Routes>
-      </Router>
+      {isProduction ? (
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </HashRouter>
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      )}
     </UserInformationProvider>
-  )
+  );
 }
 
-export default App
+export default App;
